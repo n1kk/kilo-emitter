9802b848fbc98ea7570b8ed48d041dab6e8ee991
nidy <nikita.dyomin@gmail.com>
2018-03-26 11:20:55 +0200

    added 3s6 inlined version

---
+++ dist/Emitter.es6.inlined.js
@@ -0,0 +1,103 @@
+class Emitter {
+    static extend(target) {
+        let i, emitter, keys;
+        if (target && typeof target === 'object') {
+            emitter = new Emitter();
+            ['$evt', 'on', 'off', 'once', 'emit', 'triggers']
+                .forEach((method) => { target[method] = emitter[method]; });
+        }
+        return target;
+    }
+    constructor() {
+        this.$evt = {};
+    }
+    on(event, listener, context, priority) {
+        let listeners, events = this.$evt;
+        if (event && listener) {
+            if (typeof context == 'boolean') {
+                priority = context;
+                context = null;
+            }
+            else {
+                listener.$ctx = context;
+            }
+            if (listeners = events[event]) {
+                this.off(event, listener);
+                if (priority) {
+                    listeners.unshift(listener);
+                }
+                else {
+                    listeners.push(listener);
+                }
+            }
+            else {
+                listeners = [listener];
+            }
+            events[event] = listeners;
+        }
+        return this;
+    }
+    once(event, listener, context, priority) {
+        if (event && listener) {
+            listener.$once = true;
+            this.on(event, listener, context, priority);
+        }
+        return this;
+    }
+    off(event, listener) {
+        let i, listeners, argNum = arguments.length, events = this.$evt;
+        if (argNum === 0) {
+            this.$evt = {};
+        }
+        else if (argNum === 1) {
+            delete events[event];
+        }
+        else {
+            listeners = events[event];
+            i = listeners ? listeners.indexOf(listener) : -1;
+            if (i > -1) {
+                listeners.splice(i, 1);
+                if (!listeners.length)
+                    delete events[event];
+            }
+        }
+        return this;
+    }
+    emit(event, args) {
+        let i, listener, num, listeners = this.$evt[event];
+        if (listeners && (num = listeners.length)) {
+            listeners = listeners.slice();
+            for (i = 0; i < num; i++) {
+                listener = listeners[i];
+                if (listener.apply(listener.$ctx, args) === "stopEmit")
+                    i = num;
+                if (listener.$once) {
+                    this.off(event, listener);
+                    delete listener.$once;
+                }
+            }
+        }
+        return this;
+    }
+    triggers(event, listener) {
+        let listeners, argsNum = arguments.length, events = this.$evt;
+        if (argsNum) {
+            if (listeners = events[event]) {
+                if (argsNum > 1) {
+                    return listeners.indexOf(listener) > -1;
+                }
+                else {
+                    return true;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return !!Object.getOwnPropertyNames(events).length;
+        }
+    }
+}
+
+//# sourceMappingURL=Emitter.es6.inlined.js.map
